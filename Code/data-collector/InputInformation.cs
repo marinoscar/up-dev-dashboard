8a5f4213e75bdda8720977a660ac7abc7000cdf9
Oscar Marin <oscar@marin.cr>
2018-11-21 02:42:05 -0600

    more improvements

---
+++ Code/data-collector/InputInformation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace data_collector
+{
+    public class InputInformation
+    {
+        public InputInformation()
+        {
+            Date = new DateTime(1900, 1, 1);
+            ExtractedOnUTC = DateTime.UtcNow;
+            Type = "None";
+        }
+        public string FileName { get; set; }
+        public string Type { get; set; }
+        public string ProcessName { get; set; }
+        public DateTime Date { get; set; }
+        public string DeveloperName { get; set; }
+        public string ReviwerName { get; set; }
+        public bool Failed { get; set; }
+        public string Message { get; set; }
+        public DateTime ExtractedOnUTC { get; set; }
+    }
+}
--- Code/data-collector/Tasks/ClassifyExcelFiles.cs
+++ Code/data-collector/Tasks/ClassifyExcelFiles.cs
@@ -184,23 +184,4 @@ namespace data_collector.Tasks
                 Convert.ToInt32(ds.Substring(6, 2)));
         }
     }
-
-    public class InputInformation
-    {
-        public InputInformation()
-        {
-            Date = new DateTime(1900, 1, 1);
-            ExtractedOnUTC = DateTime.UtcNow;
-            Type = "None";
-        }
-        public string FileName { get; set; }
-        public string Type { get; set; }
-        public string ProcessName { get; set; }
-        public DateTime Date { get; set; }
-        public string DeveloperName { get; set; }
-        public string ReviwerName { get; set; }
-        public bool Failed { get; set; }
-        public string Message { get; set; }
-        public DateTime ExtractedOnUTC { get; set; }
-    }
 }
